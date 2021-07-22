587916b7e9ea718a8fb0dce24ed93b91dd2e5ec5
henri <heneneri@gmail.com>
2021-07-23 00:17:48 +0300

    juukeli

--- cron.py
+++ cron.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user=True)
-job = cron.new(command='python3 ./xxx/homma.py')
+job = cron.new(command='python3 ./vmfiles/homma.py')
 job.minute.every(1)
 
 cron.write()
