6f05991b16115b292541c146a0ed062a31faf9e6
henri <heneneri@gmail.com>
2021-07-22 23:13:46 +0300

    juukeli

--- cron.py
+++ cron.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user=True)
-job = cron.new(command='python3 homma.py')
+job = cron.new(command='python3 /xxx/homma.py')
 job.minute.every(1)
 
 cron.write()
