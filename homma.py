42abdfbc1c7584fe09a9f7dd5359fa857910b36e
henri <heneneri@gmail.com>
2021-07-22 15:55:19 +0300

    juukeli

---
+++ homma.py
@@ -0,0 +1,24 @@
+import requests
+import psycopg2
+
+pw = requests.get("http://metadata.google.internal/computeMetadata/v1/instance/attributes/kek",headers={'Metadata-Flavor': 'Google'})
+sqluser = requests.get("http://metadata.google.internal/computeMetadata/v1/instance/attributes/kak",headers={'Metadata-Flavor': 'Google'})
+ip = requests.get("http://metadata.google.internal/computeMetadata/v1/instance/attributes/kok",headers={'Metadata-Flavor': 'Google'})
+
+def connect():
+    con = psycopg2.connect(host=ip,database="group3db",port=5432,user=sqluser,password=pw)
+    cursor = con.cursor()
+    select_all(cursor)
+    con.commit()
+    cursor.close()
+
+def select_all(cursor):
+
+    SQL = 'SELECT * FROM testilog;'
+    cursor.execute(SQL)
+    colnames = [desc[0] for desc in cursor.description]
+    print(colnames)
+
+if __name__ == '__main__':
+
+    connect()
